df8a070317e89593c1fb363683f84e7f0e7b7406
huseinibnu <husein.mahmud03@gmail.com>
2023-12-18 09:06:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/notessqlite/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.notessqlite
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notessqlite.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NotesDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(), this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
+    }
+}
